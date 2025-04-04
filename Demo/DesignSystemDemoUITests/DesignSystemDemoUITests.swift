4d1330363d0e77c7a2503c6ce27124d8ba0dea70
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2025-04-04 17:02:39 +0100

    chore: initial repo setup

---
+++ Demo/DesignSystemDemoUITests/DesignSystemDemoUITests.swift
@@ -0,0 +1,26 @@
+import XCTest
+
+final class DesignSystemDemoUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# mobile-ios-ui
+# Mobile Design System
+
+Use this design system to make government apps consistent with GOV.UK. Learn from the research and experience of other app teams, GOV.UK Design System and avoid repeating work that's already been done.
+
+Make services look and feel like GOV.UK using styles.
+Use pre-built reusable components to build consistent apps.
+Patterns are best practice design solutions for specific user-focused tasks and pages.
+
+
+Open Government Licence (OGL)
+https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/
