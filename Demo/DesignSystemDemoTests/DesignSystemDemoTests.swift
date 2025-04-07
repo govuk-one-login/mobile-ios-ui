7ae0fd90124543e8e629b456f349aab32b410568
Robin Phillips <104073334+robinphillips@users.noreply.github.com>
2025-04-07 12:28:31 +0100

    feat: initial repo setup (#1)
    * chore: initial repo setup
    
    * ci: revised PR template
    
    * ci: changing workspace and scheme for merged action
    
    * ci: revised gitignore
    
    ---------
    
    Co-authored-by: Dylan James <dylan.james@digital.cabinet-office.gov.uk>

---
+++ Demo/DesignSystemDemoTests/DesignSystemDemoTests.swift
@@ -0,0 +1,9 @@
+import Testing
+@testable import DesignSystemDemo
+
+struct DesignSystemDemoTests {
+
+    @Test func example() async throws {
+    }
+
+}
---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "DesignSystem",
+    products: [
+        .library(
+            name: "DesignSystem",
+            targets: ["DesignSystem"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", .upToNextMajor(from: "0.58.2"))
+    ],
+    targets: [
+        .target(
+            name: "DesignSystem",
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
+        ),
+        .testTarget(
+            name: "DesignSystemTests",
+            dependencies: ["DesignSystem"]
+        )
+    ]
+)
