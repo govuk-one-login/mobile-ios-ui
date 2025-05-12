8aead8696188147cbd87e5684b0a30b14c82789b
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2025-05-12 16:57:08 +0100

    feat: added tiny corner radius option used in expired Tags

--- Sources/DesignSystem/CornerRadius.swift
+++ Sources/DesignSystem/CornerRadius.swift
@@ -2,6 +2,7 @@ import Foundation
 
 extension DesignSystem {
     public enum CornerRadius {
+        public static let tiny: CGFloat = 2
         public static let extraSmall: CGFloat = 4
         public static let small: CGFloat = 8
         public static let `default`: CGFloat = 16
