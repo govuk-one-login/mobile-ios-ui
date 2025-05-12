ea6cf3057efda2838f2c40516bda405357c345ae
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2025-05-12 10:46:57 +0100

    feat: Added additional date formats

--- Sources/DesignSystem/DateFormats.swift
+++ Sources/DesignSystem/DateFormats.swift
@@ -2,7 +2,16 @@ import Foundation
 
 extension DesignSystem {
     public enum DateFormat {
+        /// Standard GOV.UK date format to present without day without leading zero with word month
         public static let `default`: String = "d MMMM yyyy"
+        
+        /// This is used for decoding dates with simple date string with reversed date components
         public static let reverseDecodable: String = "yyyy-MM-dd"
+        
+        /// This is used to create a Date from integer UTC date Int
+        public static let utcIntToString: String = "yyyy-MM-dd HH:mm:ss z"
+        
+        /// This is used to create a Date from UTC date string
+        public static let utcStringToDate: String = "yyyy-MM-dd'T'HH:mm:ss'Z'"
     }
 }
