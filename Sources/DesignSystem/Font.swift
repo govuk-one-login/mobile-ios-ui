be2f1150b558d66e0b8d94af179d460199174676
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2025-05-12 13:26:14 +0100

    chore: added Font tests, and updated param names to simplify

--- Sources/DesignSystem/Font.swift
+++ Sources/DesignSystem/Font.swift
@@ -5,31 +5,31 @@ extension DesignSystem {
     public enum Font {
         
         // NOTE: Example mappings from existing
-        public static let largeTitle: UIFont = .preferredFont(forTextStyle: .largeTitle)
-        public static let largeTitleBold: UIFont = .preferredFont(forTextStyle: .largeTitle, weight: .bold)
-        public static let title1: UIFont = .preferredFont(forTextStyle: .title1)
-        public static let title2: UIFont = .preferredFont(forTextStyle: .title2)
-        public static let title3: UIFont = .preferredFont(forTextStyle: .title3)
-        public static let title3Bold: UIFont = .preferredFont(forTextStyle: .title3, weight: .bold)
-        public static let headline: UIFont = .preferredFont(forTextStyle: .headline)
-        public static let subheadline: UIFont = .preferredFont(forTextStyle: .subheadline)
-        public static let body: UIFont = .preferredFont(forTextStyle: .body)
-        public static let bodySemiBold: UIFont = .preferredFont(forTextStyle: .body, weight: .semibold)
-        public static let bodyBold: UIFont = .preferredFont(forTextStyle: .body, weight: .bold)
-        public static let callout: UIFont = .preferredFont(forTextStyle: .callout)
-        public static let footnote: UIFont = .preferredFont(forTextStyle: .footnote)
-        public static let caption1: UIFont = .preferredFont(forTextStyle: .caption1)
-        public static let caption2: UIFont = .preferredFont(forTextStyle: .caption2)
+        public static let largeTitle: UIFont = .preferredFont(for: .largeTitle)
+        public static let largeTitleBold: UIFont = .preferredFont(for: .largeTitle, weight: .bold)
+        public static let title1: UIFont = .preferredFont(for: .title1)
+        public static let title2: UIFont = .preferredFont(for: .title2)
+        public static let title3: UIFont = .preferredFont(for: .title3)
+        public static let title3Bold: UIFont = .preferredFont(for: .title3, weight: .bold)
+        public static let headline: UIFont = .preferredFont(for: .headline)
+        public static let subheadline: UIFont = .preferredFont(for: .subheadline)
+        public static let body: UIFont = .preferredFont(for: .body)
+        public static let bodySemiBold: UIFont = .preferredFont(for: .body, weight: .semibold)
+        public static let bodyBold: UIFont = .preferredFont(for: .body, weight: .bold)
+        public static let callout: UIFont = .preferredFont(for: .callout)
+        public static let footnote: UIFont = .preferredFont(for: .footnote)
+        public static let caption1: UIFont = .preferredFont(for: .caption1)
+        public static let caption2: UIFont = .preferredFont(for: .caption2)
         
         // NOTE: Example new font Tokens...
-        public static let expiredTag: UIFont = .preferredFont(forTextStyle: .body, weight: .semibold, size: 12)
+        public static let expiredTag: UIFont = .preferredFont(for: .body, weight: .semibold, size: 12)
+        public static let primaryLabelLarge: UIFont = .preferredMonospacedFont(for: .title2, weight: .bold, size: 20)
     }
-    
 }
 
 extension UIFont {
     static func preferredFont(
-        forTextStyle style: UIFont.TextStyle,
+        for style: UIFont.TextStyle,
         weight: Weight = .regular,
         size: CGFloat? = nil
     ) -> UIFont {
@@ -43,12 +43,19 @@ extension UIFont {
     // NOTE: Can be made private once we have better font tokens for usage
     public static func preferredMonospacedFont(
         for style: TextStyle,
-        weight: Weight
+        weight: Weight,
+        size: CGFloat? = nil
     ) -> UIFont {
         let metrics = UIFontMetrics(forTextStyle: style)
         let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        
+        var sizeToUse: CGFloat = desc.pointSize
+        if let size = size {
+            sizeToUse = size
+        }
+        
         let font = UIFont.monospacedSystemFont(
-            ofSize: desc.pointSize,
+            ofSize: sizeToUse,
             weight: weight
         )
         return metrics.scaledFont(for: font)
