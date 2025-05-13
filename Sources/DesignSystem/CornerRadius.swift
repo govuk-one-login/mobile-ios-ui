ae8ab09c6576d7f348e339664be18e8108359db6
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2025-05-13 09:22:21 +0100

    feat: update size names

--- Sources/DesignSystem/CornerRadius.swift
+++ Sources/DesignSystem/CornerRadius.swift
@@ -2,9 +2,9 @@ import Foundation
 
 extension DesignSystem {
     public enum CornerRadius {
-        public static let tiny: CGFloat = 2
-        public static let extraSmall: CGFloat = 4
+        public static let xxSmall: CGFloat = 2
+        public static let xSmall: CGFloat = 4
         public static let small: CGFloat = 8
-        public static let `default`: CGFloat = 16
+        public static let `default`: CGFloat = 16, medium: CGFloat = 16
     }
 }
