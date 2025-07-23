e6c506c3567d3de08ab17d893ef8d33f5e90214f
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2025-07-23 22:43:28 +0100

    fix: changed initialisation of fonts to respect dynamic type correctly

--- Sources/DesignSystem/Font.swift
+++ Sources/DesignSystem/Font.swift
@@ -6,76 +6,57 @@ extension DesignSystem {
         
         // NOTE: Example mappings from existing
         public enum Base {
-            public static let largeTitle: UIFont = .preferredFont(for: .largeTitle)
-            public static let largeTitleBold: UIFont = .preferredFont(for: .largeTitle, weight: .bold)
+            public static let largeTitle: UIFont = UIFont(style: .largeTitle)
+            public static let largeTitleBold: UIFont = UIFont(style: .largeTitle, weight: .bold)
             
-            public static let title1: UIFont = .preferredFont(for: .title1)
-            public static let title1Bold: UIFont = .preferredFont(for: .title1, weight: .bold)
+            public static let title1: UIFont = UIFont(style: .title1)
+            public static let title1Bold: UIFont = UIFont(style: .title1, weight: .bold)
             
-            public static let title2: UIFont = .preferredFont(for: .title2)
-            public static let title2Bold: UIFont = .preferredFont(for: .title2, weight: .bold)
+            public static let title2: UIFont = UIFont(style: .title2)
+            public static let title2Bold: UIFont = UIFont(style: .title2, weight: .bold)
             
-            public static let title3: UIFont = .preferredFont(for: .title3)
-            public static let title3Bold: UIFont = .preferredFont(for: .title3, weight: .bold)
-            public static let title3BoldMonospaced: UIFont = .preferredMonospacedFont(for: .title3, weight: .bold)
+            public static let title3: UIFont = UIFont(style: .title3)
+            public static let title3Bold: UIFont = UIFont(style: .title3, weight: .bold)
+            public static let title3BoldMonospaced: UIFont = UIFont(style: .title3, weight: .bold, design: .monospaced)
             
-            public static let headline: UIFont = .preferredFont(for: .headline)
-            public static let headlineBold: UIFont = .preferredFont(for: .headline, weight: .bold)
+            public static let headline: UIFont = UIFont(style: .headline)
+            public static let headlineBold: UIFont = UIFont(style: .headline, weight: .bold)
             
-            public static let subheadline: UIFont = .preferredFont(for: .subheadline)
-            public static let subheadlineBold: UIFont = .preferredFont(for: .subheadline, weight: .bold)
+            public static let subheadline: UIFont = UIFont(style: .subheadline)
+            public static let subheadlineBold: UIFont = UIFont(style: .subheadline, weight: .bold)
             
-            public static let body: UIFont = .preferredFont(for: .body)
-            public static let bodySemiBold: UIFont = .preferredFont(for: .body, weight: .semibold)
-            public static let bodyBold: UIFont = .preferredFont(for: .body, weight: .bold)
+            public static let body: UIFont = UIFont(style: .body)
+            public static let bodySemiBold: UIFont = UIFont(style: .body, weight: .semibold)
+            public static let bodyBold: UIFont = UIFont(style: .body, weight: .bold)
             
-            public static let callout: UIFont = .preferredFont(for: .callout)
-            public static let calloutBold: UIFont = .preferredFont(for: .callout, weight: .bold)
+            public static let callout: UIFont = UIFont(style: .callout)
+            public static let calloutBold: UIFont = UIFont(style: .body, weight: .bold)
             
-            public static let footnote: UIFont = .preferredFont(for: .footnote)
-            public static let footnoteBold: UIFont = .preferredFont(for: .footnote, weight: .bold)
+            public static let footnote: UIFont = UIFont(style: .footnote)
+            public static let footnoteBold: UIFont = UIFont(style: .footnote, weight: .bold)
             
-            public static let caption1: UIFont = .preferredFont(for: .caption1)
-            public static let caption1Bold: UIFont = .preferredFont(for: .caption1, weight: .bold)
+            public static let caption1: UIFont = UIFont(style: .caption1)
+            public static let caption1Bold: UIFont = UIFont(style: .caption1, weight: .bold)
             
-            public static let caption2: UIFont = .preferredFont(for: .caption2)
-            public static let caption2Bold: UIFont = .preferredFont(for: .caption2, weight: .bold)
+            public static let caption2: UIFont = UIFont(style: .caption2)
+            public static let caption2Bold: UIFont = UIFont(style: .caption2, weight: .bold)
             
         }
     }
 }
 
 extension UIFont {
-    public static func preferredFont(
-        for style: UIFont.TextStyle,
+    public convenience init(
+        style: TextStyle,
         weight: Weight = .regular,
-        size: CGFloat? = nil
-    ) -> UIFont {
-        let metrics = UIFontMetrics(forTextStyle: style)
-        let descriptor = preferredFont(forTextStyle: style).fontDescriptor
-        let defaultSize = descriptor.pointSize
-        let fontToScale = UIFont.systemFont(ofSize: size ?? defaultSize, weight: weight)
-        return metrics.scaledFont(for: fontToScale)
-    }
-    
-    // NOTE: Can be made private once we have better font tokens for usage
-    public static func preferredMonospacedFont(
-        for style: TextStyle,
-        weight: Weight,
-        size: CGFloat? = nil
-    ) -> UIFont {
-        let metrics = UIFontMetrics(forTextStyle: style)
-        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
-        
-        var sizeToUse: CGFloat = desc.pointSize
-        if let size = size {
-            sizeToUse = size
+        design: UIFontDescriptor.SystemDesign = .default,
+        monospaced: Bool = false
+    ) {
+        guard let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+            .addingAttributes([.traits: [UIFontDescriptor.TraitKey.weight: weight]])
+            .withDesign(design) else {
+            preconditionFailure("Could not find a matching font")
         }
-        
-        let font = UIFont.monospacedSystemFont(
-            ofSize: sizeToUse,
-            weight: weight
-        )
-        return metrics.scaledFont(for: font)
+        self.init(descriptor: descriptor, size: 0)
     }
 }
--- Tests/DesignSystemTests/FontTests.swift
+++ Tests/DesignSystemTests/FontTests.swift
@@ -7,105 +7,105 @@ struct FontTests {
     // MARK: - Raw Fonts
     
     @Test func test_Font_largeTitle() async throws {
-        let expectedFont = UIFont.preferredFont(for: .largeTitle)
+        let expectedFont = UIFont(style: .largeTitle)
         
         let sut = DesignSystem.Font.Base.largeTitle
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_largeTitleBold() async throws {
-        let expectedFont = UIFont.preferredFont(for: .largeTitle, weight: .bold)
+        let expectedFont = UIFont(style: .largeTitle, weight: .bold)
         
         let sut = DesignSystem.Font.Base.largeTitleBold
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_title1() async throws {
-        let expectedFont = UIFont.preferredFont(for: .title1)
+        let expectedFont = UIFont(style: .title1)
         
         let sut = DesignSystem.Font.Base.title1
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_title2() async throws {
-        let expectedFont = UIFont.preferredFont(for: .title2)
+        let expectedFont = UIFont(style: .title2)
         
         let sut = DesignSystem.Font.Base.title2
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_title3() async throws {
-        let expectedFont = UIFont.preferredFont(for: .title3)
+        let expectedFont = UIFont(style: .title3)
         
         let sut = DesignSystem.Font.Base.title3
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_title3Bold() async throws {
-        let expectedFont = UIFont.preferredFont(for: .title3, weight: .bold)
+        let expectedFont = UIFont(style: .title3, weight: .bold)
         
         let sut = DesignSystem.Font.Base.title3Bold
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_headline() async throws {
-        let expectedFont = UIFont.preferredFont(for: .headline)
+        let expectedFont = UIFont(style: .headline)
         
         let sut = DesignSystem.Font.Base.headline
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_subheadline() async throws {
-        let expectedFont = UIFont.preferredFont(for: .subheadline)
+        let expectedFont = UIFont(style: .subheadline)
         
         let sut = DesignSystem.Font.Base.subheadline
         #expect(sut == expectedFont)
     }
         
     @Test func test_Font_body() async throws {
-        let expectedFont = UIFont.preferredFont(for: .body)
+        let expectedFont = UIFont(style: .body)
         
         let sut = DesignSystem.Font.Base.body
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_bodySemiBold() async throws {
-        let expectedFont = UIFont.preferredFont(for: .body, weight: .semibold)
+        let expectedFont = UIFont(style: .body, weight: .semibold)
         
         let sut = DesignSystem.Font.Base.bodySemiBold
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_bodyBold() async throws {
-        let expectedFont = UIFont.preferredFont(for: .body, weight: .bold)
+        let expectedFont = UIFont(style: .body, weight: .bold)
         
         let sut = DesignSystem.Font.Base.bodyBold
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_callout() async throws {
-        let expectedFont = UIFont.preferredFont(for: .callout)
+        let expectedFont = UIFont(style: .callout)
         
         let sut = DesignSystem.Font.Base.callout
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_footnote() async throws {
-        let expectedFont = UIFont.preferredFont(for: .footnote)
+        let expectedFont = UIFont(style: .footnote)
         
         let sut = DesignSystem.Font.Base.footnote
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_caption1() async throws {
-        let expectedFont = UIFont.preferredFont(for: .caption1)
+        let expectedFont = UIFont(style: .caption1)
         
         let sut = DesignSystem.Font.Base.caption1
         #expect(sut == expectedFont)
     }
     
     @Test func test_Font_caption2() async throws {
-        let expectedFont = UIFont.preferredFont(for: .caption2)
+        let expectedFont = UIFont(style: .caption2)
         
         let sut = DesignSystem.Font.Base.caption2
         #expect(sut == expectedFont)
@@ -113,7 +113,7 @@ struct FontTests {
 
     // MARK: - Font Helpers
     @Test func test_Font_preferredMonospacedFont() async throws {
-        let expectedFont = UIFont.preferredMonospacedFont(for: .title3, weight: .bold)
+        let expectedFont = UIFont(style: .title3, weight: .bold, design: .monospaced)
         
         let sut = DesignSystem.Font.Base.title3BoldMonospaced
         #expect(sut == expectedFont)
