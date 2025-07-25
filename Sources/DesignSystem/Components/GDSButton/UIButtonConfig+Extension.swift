078fda08348687340400e6071f10616c3cc38e03
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2025-07-25 09:03:51 +0100

    feat: added function to create new style from existing

--- Sources/DesignSystem/Components/GDSButton/UIButtonConfig+Extension.swift
+++ Sources/DesignSystem/Components/GDSButton/UIButtonConfig+Extension.swift
@@ -1,14 +1,7 @@
 import UIKit
 
 extension UIButton.Configuration {
-    static var primary: Self {
-        var config = UIButton.Configuration.borderedProminent()
-        config.imagePlacement = .top
-        config.imagePadding = .zero
-        return config
-    }
-    
-    static var secondary: Self {
+    static var gds: Self {
         var config = UIButton.Configuration.borderedProminent()
         config.imagePlacement = .top
         return config
