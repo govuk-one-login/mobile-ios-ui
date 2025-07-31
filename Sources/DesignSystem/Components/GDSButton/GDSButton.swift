1c30a5c724deac5d80b03812be2eb359b0f8ee7a
Robin Phillips <104073334+robinphillips@users.noreply.github.com>
2025-07-31 16:38:20 +0100

    feat: added voiceover focus (#18)

--- Sources/DesignSystem/Components/GDSButton/GDSButton.swift
+++ Sources/DesignSystem/Components/GDSButton/GDSButton.swift
@@ -105,4 +105,17 @@ public final class GDSButton: UIButton {
             return false
         }
     }
+    
+    public override func accessibilityElementDidLoseFocus() {
+        super.accessibilityElementDidLoseFocus()
+        self.setNeedsUpdateConfiguration()
+    }
+    
+    public override func accessibilityElementDidBecomeFocused() {
+        super.accessibilityElementDidBecomeFocused()
+        var config = self.configuration
+        config?.baseBackgroundColor = viewModel.style.backgroundColor.forState(.focused)
+        config?.baseForegroundColor = viewModel.style.foregroundColor.forState(.focused)
+        self.configuration = config
+    }
 }
