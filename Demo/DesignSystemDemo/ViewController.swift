a04305323ef35fdd7528a4d5954115e06cc1aa62
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-08-12 19:45:22 +0100

    chore: minor enhancements

--- Demo/DesignSystemDemo/ViewController.swift
+++ Demo/DesignSystemDemo/ViewController.swift
@@ -45,8 +45,7 @@ class ViewController: UIViewController {
                         )
                     ],
                     showShadow: true,
-                    dismissAction: {
-                    }
+                    dismissAction: .action({ })
                 )
             ],
             moveableFooter: [],
