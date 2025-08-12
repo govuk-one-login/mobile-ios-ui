6176fabccf481b3a61120972a2c6fd55ffeab051
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-08-12 12:44:31 +0100

    chore: adding additional views for content tile

---
+++ Sources/DesignSystem/Components/GDSContentCard/DividerViewModel.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+public struct DividerViewModel {
+    let height: CGFloat
+    let colour: UIColor
+    public let verticalPadding: VerticalPadding?
+    public let horizontalPadding: HorizontalPadding?
+    
+    public init(
+        height: CGFloat,
+        colour: UIColor,
+        verticalPadding: VerticalPadding? = .vertical(8),
+        horizontalPadding: HorizontalPadding? = .trailing(0)
+    ) {
+        self.height = height
+        self.colour = colour
+        self.verticalPadding = verticalPadding
+        self.horizontalPadding = horizontalPadding
+    }
+}
+
+extension DividerViewModel: ContentItem {
+    public var uiView: UIView {
+        let view = UIView()
+        view.backgroundColor = colour
+        view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        view.accessibilityIdentifier = "divider"
+        return view
+    }
+}
