78e55b531fd48d983e9acadad569218a5c03f2ff
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-08-13 12:14:53 +0100

    test: adding tests and removing redundant code

--- Sources/DesignSystem/Extensions/UIStackView+Extensions.swift
+++ Sources/DesignSystem/Extensions/UIStackView+Extensions.swift
@@ -14,18 +14,4 @@ extension UIStackView {
         self.alignment = alignment
         self.distribution = distribution
     }
-    
-    public convenience init(
-        views: [UIView],
-        axis: NSLayoutConstraint.Axis = .vertical,
-        spacing: CGFloat = 24,
-        alignment: UIStackView.Alignment = .fill,
-        distribution: UIStackView.Distribution = .fillEqually
-    ) {
-        self.init(arrangedSubviews: views)
-        self.axis = axis
-        self.spacing = spacing
-        self.alignment = alignment
-        self.distribution = distribution
-    }
 }
