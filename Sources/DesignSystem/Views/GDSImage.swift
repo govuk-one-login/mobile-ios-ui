1736ce10ea95756224352b06be9a27969912e3e1
Joe <joe.mcgarry@digital.cabinet-office.gov.uk>
2025-11-17 10:24:10 +0000

    fix: DCMAW-14691 adjusting GDSImageView constraints (#39)
    * fix: changes constraint from greaterThanOrEqualTo to lessThanOrEqualTo

--- Sources/DesignSystem/Views/GDSImage.swift
+++ Sources/DesignSystem/Views/GDSImage.swift
@@ -60,7 +60,7 @@ public final class GDSImageView: UIImageView, ContentView {
         
         if viewModel.contentMode == .scaleAspectFit {
             if let imageHeightConstraint = viewModel.imageHeightConstraint {
-                heightAnchor.constraint(greaterThanOrEqualToConstant: imageHeightConstraint).isActive = true
+                heightAnchor.constraint(lessThanOrEqualToConstant: imageHeightConstraint).isActive = true
             } else {
                 heightAnchor.constraint(equalTo: widthAnchor, multiplier: viewModel.aspectRatio).isActive = true
             }
