709f594c307317c821f1ea09457d174df0252e2d
Ifrah Hussein <ifrah.hussein@digital.cabinet-office.gov.uk>
2025-11-21 13:41:37 +0000

    feat: adds concrete type for ListViewModel

--- Demo/DesignSystemDemo/ViewController.swift
+++ Demo/DesignSystemDemo/ViewController.swift
@@ -11,82 +11,111 @@ class ViewController: UIViewController {
     var viewModel: TestViewControllerViewModel {
         TestViewControllerViewModel(
             body: [
-                GDSCardViewModel(
-                    showShadow: true,
-                    dismissAction: .action({ })
-                ) {
-                    GDSCardImageViewModel(
-                        image: UIImage(named: "placeholder") ?? UIImage(),
-                        contentMode: .scaleAspectFit
-                    )
-                    GDSCardTextViewModel(
-                        title: GDSLocalisedString(
-                            stringLiteral: "Here is the caption for the picture",
-                            stringAttributes: [("Here is the caption for the picture",
-                                                [.foregroundColor: DesignSystem.Color.Icons.success])]
-                        ),
-                        verticalPadding: .vertical(8)
-                    )
-                    GDSCardTextViewModel(
-                        title: "A title for the component for a quick introduction",
-                        titleFont: DesignSystem.Font.Base.title1Bold,
-                        verticalPadding: .bottom(8)
-                    )
-                    GDSCardTextViewModel(
-                        title: "A subtitle for the componenet which can be used to describe it's purpose",
-                        verticalPadding: .bottom(8)
-                    )
-                    GDSCardDividerViewModel(
-                        verticalPadding: .bottom(8)
-                    )
-                    GDSButtonViewModel(
-                        title: "Secondary Button",
-                        style: .secondary,
-                        buttonAction: .action({ }),
-                        verticalPadding: .bottom(8),
-                        horizontalPadding: .horizontal(16)
-                    )
-                    GDSButtonViewModel(
-                        title: "Primary Button",
-                        style: .primary,
-                        buttonAction: .action({ }),
-                        verticalPadding: .bottom(16),
-                        horizontalPadding: .horizontal(16)
-                    )
-                },
-                GDSCardViewModel(
-                    showShadow: true,
-                    dismissAction: .action({ })
-                ) {
-                    GDSCardTitleViewModel(
-                        title: "A title for the component",
-                        verticalPadding: .bottom(8),
-                        horizontalPadding: .leading(16)
-                    )
-                    GDSCardTextViewModel(
-                        title: "A subtitle for the componenet which can be used to describe it's purpose",
-                        verticalPadding: .bottom(8)
-                    )
-                    GDSCardDividerViewModel(
-                        verticalPadding: .bottom(8)
-                    )
-                    GDSButtonViewModel(
-                        title: "Secondary Button",
-                        icon: .arrowUpRight,
-                        style: .secondary.adjusting(
-                            alignment: .leading,
-                            contentInsets: NSDirectionalEdgeInsets(
-                                top: DesignSystem.Spacing.small,
-                                leading: .zero,
-                                bottom: DesignSystem.Spacing.small,
-                                trailing: DesignSystem.Spacing.default
-                            )
-                        ),
-                        buttonAction: .action({ }),
-                        verticalPadding: .bottom(8),
-                        horizontalPadding: .horizontal(16)
-                    )
-                }
+//                GDSCardViewModel(
+//                    showShadow: true,
+//                    dismissAction: .action({ })
+//                ) {
+//                    GDSCardImageViewModel(
+//                        image: UIImage(named: "placeholder") ?? UIImage(),
+//                        contentMode: .scaleAspectFit
+//                    )
+//                    GDSCardTextViewModel(
+//                        title: GDSLocalisedString(
+//                            stringLiteral: "Here is the caption for the picture",
+//                            stringAttributes: [("Here is the caption for the picture",
+//                                                [.foregroundColor: DesignSystem.Color.Icons.success])]
+//                        ),
+//                        verticalPadding: .vertical(8)
+//                    )
+//                    GDSCardTextViewModel(
+//                        title: "A title for the component for a quick introduction",
+//                        titleFont: DesignSystem.Font.Base.title1Bold,
+//                        verticalPadding: .bottom(8)
+//                    )
+//                    GDSCardTextViewModel(
+//                        title: "A subtitle for the componenet which can be used to describe it's purpose",
+//                        verticalPadding: .bottom(8)
+//                    )
+//                    GDSCardDividerViewModel(
+//                        verticalPadding: .bottom(8)
+//                    )
+//                    GDSButtonViewModel(
+//                        title: "Secondary Button",
+//                        style: .secondary,
+//                        buttonAction: .action({ }),
+//                        verticalPadding: .bottom(8),
+//                        horizontalPadding: .horizontal(16)
+//                    )
+//                    GDSButtonViewModel(
+//                        title: "Primary Button",
+//                        style: .primary,
+//                        buttonAction: .action({ }),
+//                        verticalPadding: .bottom(16),
+//                        horizontalPadding: .horizontal(16)
+//                    )
+//                },
+//                GDSCardViewModel(
+//                    showShadow: true,
+//                    dismissAction: .action({ })
+//                ) {
+//                    GDSCardTitleViewModel(
+//                        title: "A title for the component",
+//                        verticalPadding: .bottom(8),
+//                        horizontalPadding: .leading(16)
+//                    )
+//                    GDSCardTextViewModel(
+//                        title: "A subtitle for the componenet which can be used to describe it's purpose",
+//                        verticalPadding: .bottom(8)
+//                    )
+//                    GDSCardDividerViewModel(
+//                        verticalPadding: .bottom(8)
+//                    )
+//                    GDSButtonViewModel(
+//                        title: "Secondary Button",
+//                        icon: .arrowUpRight,
+//                        style: .secondary.adjusting(
+//                            alignment: .leading,
+//                            contentInsets: NSDirectionalEdgeInsets(
+//                                top: DesignSystem.Spacing.small,
+//                                leading: .zero,
+//                                bottom: DesignSystem.Spacing.small,
+//                                trailing: DesignSystem.Spacing.default
+//                            )
+//                        ),
+//                        buttonAction: .action({ }),
+//                        verticalPadding: .bottom(8),
+//                        horizontalPadding: .horizontal(16)
+//                    )
+//                },
+                DefaultListViewModel(
+                    title: "Numbered List",
+                    titleConfig: (font: UIFont.preferredFont(forTextStyle: .body), isHeader: true),
+                    items: [
+                        "Item 1",
+                        "Item 2",
+                        "Item 3"
+                    ],
+                    style: .numbered
+                ),
+                DefaultListViewModel(
+                    title: "Bulleted List",
+                    titleConfig: (font: UIFont.preferredFont(forTextStyle: .headline), isHeader: true),
+                    items: [
+                        "Item 1",
+                        "Item 2",
+                        "Item 3"
+                    ],
+                    style: .bulleted
+                ),
+                DefaultListViewModel(
+                    titleConfig: (font: UIFont.preferredFont(forTextStyle: .body), isHeader: false),
+                    items: [
+                        "Item 1 - this is an example of a numbered list without a title, long texts should wrap this is an example of a numbered list without a title, long texts should wrap",
+                        "Item 2",
+                        "Item 3"
+                    ],
+                    style: .numbered
+                ),
             ],
             moveableFooter: [],
             footer: []
--- Sources/DesignSystem/Components/ListViewModel.swift
+++ Sources/DesignSystem/Components/ListViewModel.swift
@@ -14,3 +14,34 @@ public protocol ListViewModel: ContentViewModel {
     var items: [GDSLocalisedString] { get }
     var style: ListStyle { get }
 }
+
+public struct DefaultListViewModel: ListViewModel {
+    public typealias ViewType = ListView<DefaultListViewModel>
+        
+    public var title: GDSLocalisedString?
+    public var titleConfig: TitleConfig?
+    public var items: [GDSLocalisedString]
+    public var style: ListStyle
+    public var verticalPadding: VerticalPadding?
+    public var horizontalPadding: HorizontalPadding?
+    
+    public init(
+        title: GDSLocalisedString? = nil,
+        titleConfig: TitleConfig? = nil,
+        items: [GDSLocalisedString],
+        style: ListStyle,
+        verticalPadding: VerticalPadding? = nil,
+        horizontal: HorizontalPadding? = nil,
+    ) {
+        self.title = title
+        self.titleConfig = titleConfig
+        self.items = items
+        self.style = style
+        self.verticalPadding = verticalPadding
+        self.horizontalPadding = horizontal
+    }
+    
+    public func createUIView() -> UIView {
+        ListView(viewModel: self)
+    }
+}
