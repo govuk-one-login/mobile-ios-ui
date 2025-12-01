d46328cd51dfe99be8384ce6d231a514359f06a3
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2025-12-01 15:53:27 +0000

    fix: added new images

--- Tests/DesignSystemTests/TestExtensions/SnapshotTesting+Extensions.swift
+++ Tests/DesignSystemTests/TestExtensions/SnapshotTesting+Extensions.swift
@@ -161,7 +161,7 @@ extension View {
 
 extension [ViewImageConfig] {
     public static let standard: [ViewImageConfig] = [
-        .iPhone13ProMax(.portrait),
-        .iPhone13ProMax(.landscape)
+        .iPhone13(.portrait),
+        .iPhone13(.landscape)
     ]
 }
