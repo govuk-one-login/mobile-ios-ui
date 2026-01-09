d26df4bb6cff4a39a8f89bfb20c6a4ae8eef88d4
mradamfrancis <ajfrancis@gmail.com>
2026-01-09 14:29:41 +0000

    Add perform methods to Action enum
    This will assist running tests where Button actions need to be invoked
    
    Introduces perform() and performAsync() methods to the DesignSystem.Action enum for executing synchronous and asynchronous actions, respectively.

--- Sources/DesignSystem/Action.swift
+++ Sources/DesignSystem/Action.swift
@@ -2,6 +2,24 @@ extension DesignSystem {
     public enum Action {
         case action(() -> Void)
         case asyncAction(() async -> Void)
+        
+        public func perform() {
+            switch self {
+            case .action(let action):
+                action()
+            case .asyncAction:
+                assertionFailure("Use performAsync() for async actions")
+            }
+        }
+        
+        public func performAsync() async {
+            switch self {
+            case .action(let action):
+                action()
+            case .asyncAction(let action):
+                await action()
+            }
+        }
     }
 }
 
