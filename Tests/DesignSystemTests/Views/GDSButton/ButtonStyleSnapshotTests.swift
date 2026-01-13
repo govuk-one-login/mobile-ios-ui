f061ff6aa1b4966f412e543a4f03bf0b96ddfa7c
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2026-01-13 12:35:40 +0000

    fix: Added highlighted and disable states

--- Tests/DesignSystemTests/Views/GDSButton/ButtonStyleSnapshotTests.swift
+++ Tests/DesignSystemTests/Views/GDSButton/ButtonStyleSnapshotTests.swift
@@ -8,38 +8,170 @@ struct ButtonStyleSnapshotTests {
     
     @Test
     func desctructiveButton() {
-        let vc = createButton(GDSButtonStyle.destructive, "Destructive")
-        vc.assertSnapshot(in: size)
+        if let vc = createButton(
+            GDSButtonStyle.destructive,
+            "Destructive"
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func desctructiveButtonHighlighted() {
+        if let vc = createButton(
+            GDSButtonStyle.destructive,
+            "Destructive",
+            highlighted: true
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func desctructiveButtonDisabled() {
+        if let vc = createButton(
+            GDSButtonStyle.destructive,
+            "Destructive",
+            enabled: false
+        ) {
+            vc.assertSnapshot(in: size)
+        }
     }
     
     @Test
     func dismissButton() {
-        let vc = createButton(GDSButtonStyle.dismiss, "Dismiss")
-        vc.assertSnapshot(in: size)
+        if let vc = createButton(
+            GDSButtonStyle.dismiss,
+            "Dismiss"
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func dismissButtonHighlighted() {
+        if let vc = createButton(
+            GDSButtonStyle.dismiss,
+            "Dismiss",
+            highlighted: true
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func dismissButtonDisabled() {
+        if let vc = createButton(
+            GDSButtonStyle.dismiss,
+            "Dismiss",
+            enabled: false
+        ) {
+            vc.assertSnapshot(in: size)
+        }
     }
     
     @Test
     func primaryButton() {
-        let vc = createButton(GDSButtonStyle.primary, "Primary")
-        vc.assertSnapshot(in: size)
+        if let vc = createButton(
+            GDSButtonStyle.primary,
+            "Primary"
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func primaryButtonHighlighted() {
+        if let vc = createButton(
+            GDSButtonStyle.primary,
+            "Primary",
+            highlighted: true
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func primaryButtonDisabled() {
+        if let vc = createButton(
+            GDSButtonStyle.primary,
+            "Primary",
+            enabled: false
+        ) {
+            vc.assertSnapshot(in: size)
+        }
     }
     
     @Test
     func secondaryButton() {
-        let vc = createButton(GDSButtonStyle.secondary, "Secondary")
-        vc.assertSnapshot(in: size)
+        if let vc = createButton(
+            GDSButtonStyle.secondary,
+            "Secondary"
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func secondaryButtonHighlighted() {
+        if let vc = createButton(
+            GDSButtonStyle.secondary,
+            "Secondary",
+            highlighted: true
+        ) {
+            vc.assertSnapshot(in: size)
+        }
+    }
+    
+    @Test
+    func secondaryButtonDisabled() {
+        if let vc = createButton(
+            GDSButtonStyle.secondary,
+            "Secondary",
+            enabled: false
+        ) {
+            vc.assertSnapshot(in: size)
+        }
     }
     
     @Test
     func secondaryDestructiveButton() {
-        let vc = createButton(GDSButtonStyle.secondaryDestructive, "Secondary destructive")
-        vc.assertSnapshot(in: CGSize(width: 600, height: 100))
+        if let vc = createButton(
+            GDSButtonStyle.secondaryDestructive,
+            "Secondary destructive"
+        ) {
+            vc.assertSnapshot(in: CGSize(width: 600, height: 100))
+        }
+    }
+    
+    @Test
+    func secondaryDestructiveButtonHighlighted() {
+        if let vc = createButton(
+            GDSButtonStyle.secondaryDestructive,
+            "Secondary destructive",
+            highlighted: true
+        ) {
+            vc.assertSnapshot(in: CGSize(width: 600, height: 100))
+        }
+    }
+    
+    @Test
+    func secondaryDestructiveButtonDisabled() {
+        if let vc = createButton(
+            GDSButtonStyle.secondaryDestructive,
+            "Secondary destructive",
+            enabled: false
+        ) {
+            vc.assertSnapshot(in: CGSize(width: 600, height: 100))
+        }
     }
     
     func createButton(
         _ style: GDSButtonStyle,
-        _ styleName: String
-    ) -> UIView {
+        _ styleName: String,
+        highlighted: Bool = false,
+        enabled: Bool = true
+    ) -> UIView? {
         let viewModel = GDSButtonViewModel(
             title: "\(styleName)",
             icon: IconStyle.arrowUpRight,
@@ -50,6 +182,9 @@ struct ButtonStyleSnapshotTests {
             horizontalPadding: .horizontal(DesignSystem.Spacing.default)
         )
         
-        return viewModel.createUIView()
+        let button = viewModel.createUIView() as? UIButton
+        button?.isHighlighted = highlighted
+        button?.isEnabled = enabled
+        return button
     }
 }
