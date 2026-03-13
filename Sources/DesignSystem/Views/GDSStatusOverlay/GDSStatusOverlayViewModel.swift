255326a1f5555ed199088ae3ebf1b6f083f4fb79
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-03-13 11:55:40 +0000

    feat: create status overlay component

---
+++ Sources/DesignSystem/Views/GDSStatusOverlay/GDSStatusOverlayViewModel.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+/// Creates a Status Overlay component
+///
+/// - Parameters:
+///   - iconStyle: The style of the icon to display, defaulted to `.statusOverlay`.
+///   - warningText: Type `GDSTextViewModel`, use font `bodySemiBold`, color: `statusOverlay` and alignment `centered`
+/// - remark: The position defined in IconStyle will not be respected by this component and will always be centered.
+public struct GDSStatusOverlayViewModel: ContentViewModel {
+    public typealias ViewType = GDSStatusOverlay
+    
+    let statusOverlayText: GDSTextViewModel
+    let iconStyle: IconStyle?
+    
+    public var verticalPadding: VerticalPadding? = .vertical(DesignSystem.Spacing.default)
+    public var horizontalPadding: HorizontalPadding? = .horizontal(DesignSystem.Spacing.default)
+    
+    public var accessibilityLabel: String {
+        "\(statusOverlayText.title)"
+    }
+    
+    public init(
+        iconStyle: IconStyle? = .statusOverlay,
+        statusText: GDSTextViewModel
+    ) {
+        self.iconStyle = iconStyle
+        self.statusOverlayText = statusText
+    }
+}
