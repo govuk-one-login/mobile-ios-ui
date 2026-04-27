88bbf2d24c8308cb70b3613e0e14fa0230acd01c
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-04-27 10:17:29 +0100

    chore: DCMAW-18901 tidy file structure (#87)
    * chore: remove parent DesignSystem folder within Sources
    
    * chore: remove DesignSystemTests parent folder
    
    * chore: rename Views to Components
    
    * chore: rename screens to patterns
    
    * chore: create Styles folder
    
    * chore: create utilities folder
    
    * chore: update package file
    
    * chore: separate views and viewModels
    
    * chore: tidy tests
    
    * chore: delete unused mockView Controller
    
    * chore: import ui kit

---
+++ Sources/Components/GDSImage/GDSImageViewModel.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+@available(*, deprecated, renamed: "GDSImageViewModel", message: "to be removed October 2025")
+public typealias GDSCardImageViewModel = GDSImageViewModel
+
+public struct GDSImageViewModel: ContentViewModel {
+    public typealias ViewType = GDSImageView
+    
+    let image: UIImage
+    let imageColour: UIColor?
+    let contentMode: UIView.ContentMode
+    let imageHeightConstraint: CGFloat?
+    let cornerMask: CACornerMask?
+    let accessibilityLabel: String?
+    
+    public let verticalPadding: VerticalPadding?
+    public let horizontalPadding: HorizontalPadding?
+    
+    var aspectRatio: CGFloat {
+        max(image.size.height, 1) / max(image.size.width, 1)
+    }
+    
+    public init(
+        image: UIImage,
+        imageColour: UIColor? = nil,
+        contentMode: UIView.ContentMode = .scaleAspectFit,
+        imageHeightConstraint: CGFloat? = nil,
+        cornerMask: CACornerMask? = nil,
+        accessibilityLabel: String? = nil,
+        verticalPadding: VerticalPadding? = .vertical(0),
+        horizontalPadding: HorizontalPadding? = .horizontal(0)
+    ) {
+        self.image = image
+        self.imageColour = imageColour
+        self.contentMode = contentMode
+        self.imageHeightConstraint = imageHeightConstraint
+        self.cornerMask = cornerMask
+        self.accessibilityLabel = accessibilityLabel
+        self.verticalPadding = verticalPadding
+        self.horizontalPadding = horizontalPadding
+    }
+}
--- Sources/Components/GDSText/GDSTextViewModel.swift
+++ Sources/Components/GDSText/GDSTextViewModel.swift
@@ -33,32 +33,3 @@ public struct GDSTextViewModel: ContentViewModel {
         self.horizontalPadding = horizontalPadding
     }
 }
-
-@available(*, deprecated, renamed: "GDSTextView", message: "to be removed October 2025")
-public typealias GDSCardTextView = GDSTextView
-
-public final class GDSTextView: UILabel, ContentView {
-    public init(viewModel: GDSTextViewModel) {
-        super.init(frame: .zero)
-        
-        self.font = viewModel.titleFont
-        self.textColor = viewModel.textColor
-        if let attributedTitle = viewModel.title.attributedValue {
-            self.attributedText = attributedTitle
-        } else {
-            self.text = viewModel.title.value
-        }
-        self.textAlignment = viewModel.alignment
-        self.numberOfLines = .zero
-        self.lineBreakMode = .byWordWrapping
-        self.adjustsFontForContentSizeCategory = true
-        if let accessibilityTraits = viewModel.accessibilityTraits {
-            self.accessibilityTraits = accessibilityTraits
-        }
-        self.accessibilityIdentifier = "gds-text-view"
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
