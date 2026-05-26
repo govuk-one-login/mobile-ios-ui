90cf7b08f3a56fb4b86996eda13c1d494ef598d8
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2026-05-26 10:43:49 +0100

    feat: DCMAW-19383 add red6 (#95)
    * feat: add red6
    
    * ci: change xcode and simulator version
    
    * ci: making updates to post merge workflow and adding arch=arm64 for multiple matching destinations

--- Sources/Styles/Colors.swift
+++ Sources/Styles/Colors.swift
@@ -192,6 +192,7 @@ extension DesignSystem {
             public static let red3 = UIColor.fromHex("#55150B")
             public static let red4 = UIColor.fromHex("#9F2815")
             public static let red5 = UIColor.fromHex("#CC544E")
+            public static let red6 = UIColor.fromHex("#F4D7D7")
             
             public static let pink1 = UIColor.fromHex("#F7DEE5")
             public static let pink2 = UIColor.fromHex("#FFF4FD")
