f44c76e1b0083a999dded696b56387fac0b7785d
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-06-10 13:35:39 +0100

    feat: add preventModalDismiss in BaseViewModel

--- Sources/Patterns/BaseScreen.swift
+++ Sources/Patterns/BaseScreen.swift
@@ -48,6 +48,8 @@ open class BaseScreen: UIViewController {
                                                            action: #selector(dismissScreen))
             self.navigationItem.rightBarButtonItem?.accessibilityIdentifier = "right-bar-button"
         }
+        
+        isModalInPresentation = viewModel?.preventModalDismiss ?? false
     }
     
     open override func viewIsAppearing(_ animated: Bool) {
