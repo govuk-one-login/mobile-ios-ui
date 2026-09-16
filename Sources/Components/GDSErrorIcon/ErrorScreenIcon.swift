57a357536a0ae87e3e72611de8168ac18a3786a7
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-09-16 09:39:33 +0100

    feat: add update case to ErrorScreenIcon

--- Sources/Components/GDSErrorIcon/ErrorScreenIcon.swift
+++ Sources/Components/GDSErrorIcon/ErrorScreenIcon.swift
@@ -1,17 +1,20 @@
 public enum ErrorScreenIcon {
     case error
     case warning
+    case update
     
     var iconName: String {
         switch self {
         case .error, .warning:
             "exclamationmark.circle"
+        case .update:
+            "exclamationmark.arrow.circlepath"
         }
     }
     
     var voiceoverPrefix: String {
         switch self {
-        case .error:
+        case .error, .update:
             NSLocalizedString(key: "voiceOverErrorPrefix", bundle: .designSystem)
         case .warning:
             NSLocalizedString(key: "voiceOverWarningPrefix", bundle: .designSystem)
