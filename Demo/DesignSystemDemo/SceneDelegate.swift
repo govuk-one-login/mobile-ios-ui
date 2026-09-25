738a364253a9492e67317ce20dadff6bb613629e
joshdubey <95635393+joshdubey@users.noreply.github.com>
2026-09-25 09:59:10 +0100

    feat:  DCMAW-22779: Add background colors to GDSScreen's main view and footer component (#135)
    * feat:  Add background colour to GDSScreen view and footer..
    
    * chore:  Add demo screen for background colours, and tests.

--- Demo/DesignSystemDemo/SceneDelegate.swift
+++ Demo/DesignSystemDemo/SceneDelegate.swift
@@ -24,16 +24,22 @@ struct GDSDemoScreenViewModel: GDSScreenViewModel {
     public let body: [any ContentViewModel]
     public let movableFooter: [any ContentViewModel]
     public let footer: [any ContentViewModel]
+    public let backgroundColor: UIColor?
+    public let footerBackgroundColor: UIColor?
     
     public init(
         screenStyle: GDSScreenStyle = .top,
         body: [any ContentViewModel],
         movableFooter: [any ContentViewModel],
-        footer: [any ContentViewModel]
+        footer: [any ContentViewModel],
+        backgroundColor: UIColor? = .systemBackground,
+        footerBackgroundColor: UIColor? = nil
     ) {
         self.screenStyle = screenStyle
         self.body = body
         self.movableFooter = movableFooter
         self.footer = footer
+        self.backgroundColor = backgroundColor
+        self.footerBackgroundColor = footerBackgroundColor
     }
 }
